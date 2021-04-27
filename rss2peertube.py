0a6f4de1df5585f0a249fe2ca0eee2fe628dfb2b
dhk2 <donkimberlin@hotmail.com>
2021-04-27 03:55:28 -0700

    a

--- rss2peertube.py
+++ rss2peertube.py
@@ -145,7 +145,8 @@ def run_steps(conf):
                         published = str(utils.convert_timestamp(p))
                     title = queue_item["title"]
                     title = title.replace(",",".")
-                    title = title.replace("&#x27","'")
+                    title = title.replace("&#x27;","'")
+                    title = title.replace("&quot;","'")
                     print("title:"+title)
                     file = open ("videos.log","a+")
                     file.write(channel_conf["name"]+","+published+","+title+"\n")

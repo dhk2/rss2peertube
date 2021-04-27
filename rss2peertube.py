e71e4d85b9bc88e98744cfa11076cee2ffa258e0
dhk2 <donkimberlin@hotmail.com>
2021-04-26 22:49:49 -0700

    logging

--- rss2peertube.py
+++ rss2peertube.py
@@ -153,7 +153,7 @@ def run(run_once=True):
 
 
 def main(argv):
-  logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+  logging.basicConfig(filename='example.log', level=logging.DEBUG)
   run_once=False
   try:
     opts, args = getopt.getopt(argv,"hor",["help","once","reset"])
